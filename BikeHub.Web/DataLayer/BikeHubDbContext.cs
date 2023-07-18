403913dcc275041c64b62e7a416d2b7133ea0c27
Shoukat Malik <shoukatmalik.dev@gmail.com>
2023-07-19 00:48:54 +0500

    Add project files.

---
+++ BikeHub.Web/DataLayer/BikeHubDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BikeHub.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BikeHub.Web.DataLayer
+{
+    public class BikeHubDbContext : DbContext
+    {
+        public BikeHubDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Make> Makes { get; set; }
+        public DbSet<Model> Models { get; set; }
+    }
+}
---
+++ BikeHub.Web/Models/Domain/Model.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BikeHub.Web.Models.Domain
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }=string.Empty;
+        [ForeignKey("Make")]
+        public int MakeId { get; set; }
+        public Make Make { get; set; }
+    }
+}
